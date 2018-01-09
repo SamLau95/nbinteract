841281c66b3c39c422b4b7de19b6381599922e22
Caleb Siu <calebs11@berkeley.edu>
2018-01-09 00:23:11 -0500

    Added interactive questions and added function parem to short answer

--- nbinteract/questions.py
+++ nbinteract/questions.py
@@ -7,7 +7,7 @@ __all__ = ['multiple_choice', 'short_answer']
 TEXT_STYLE = '<p style="line-height:18px;">{}</p>'
 
 
-def multiple_choice(question, answer_list, correct_answer):
+def multiple_choice(question, answer_list, correct_answer_list):
     """
     Generates a multiple choice question that allows the user to select an
     answer choice and displays the correctness of that choice.
@@ -17,8 +17,8 @@ def multiple_choice(question, answer_list, correct_answer):
         The question being asked.
         ''answer_list'' -- (list of strings)
         The possible answer choices.
-        ''correct_answeer'' -- (string)
-        The correct answer for the question.
+        ''correct_answeer_list'' -- (string)
+        List of all correct answers.
 
     Returns:
         None
@@ -38,7 +38,7 @@ def multiple_choice(question, answer_list, correct_answer):
 
     # interactive function that turns button green if correct and red otherwise
     def check_answer(answer_content):
-        if button_answer_mapping[answer_content] == correct_answer:
+        if button_answer_mapping[answer_content] in correct_answer_list:
             answer_content.style.button_color = '#71B307'
         else:
             answer_content.style.button_color = '#F86700'
@@ -65,7 +65,7 @@ X_ICON = ('<i class="fa fa-times" aria-hidden="true"'
           'style="color:#F86700; font-size:2rem"></i>')
 
 
-def short_answer(question, answer, explanation=None):
+def short_answer(question, answer_list=[], check_function=lambda x: False, explanation=None):
     """
     Generates a short answer question that allows user to input an answer in
     a textbox and a submit button to output the correctness of the answer.
@@ -74,7 +74,7 @@ def short_answer(question, answer, explanation=None):
         ''question'' --(string)
         The question being asked.
         ''answer'' --(string)
-        The correct answer to the question.
+        A list of correct answers.
         ''explanation'' --(string)
         The explanation to the question is displayed when the user inputs
         the correct answer.
@@ -97,7 +97,11 @@ def short_answer(question, answer, explanation=None):
 
     # correctness function linked to the submit button
     def check_answer(text_content):
-        if user_answer.value == str(answer):
+        try:
+            user_answer_value = int(user_answer.value)
+        except ValueError:
+            user_answer_value = user_answer.value
+        if user_answer_value in answer_list or check_function(user_answer_value):
             visual_correct.value = CHECK_ICON
             if explanation:
                 explanation_space.value = explanation
