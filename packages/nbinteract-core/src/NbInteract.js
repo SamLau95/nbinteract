9b054c6d7b831f4d2aa98a3d48c25502f32706c0
Sam Lau <samlau95@gmail.com>
2018-01-13 02:06:21 -0800

    Allow gitbook plugin to be manually run
    This PR allows the gitbook plugin to take in the following config:
    
    ```json
    "spec": {
      "type": "String",
      "description": [
        "Spec for the BinderHub image. Must be in the format ",
        "${username}/${repo}/${branch}. By default uses the nbinteract ",
        "image."
      ],
      "default": "SamLau95/nbinteract-image/master"
    },
    "provider": {
      "type": "String",
      "description": [
        "Provider for the BinderHub image. ",
        "By default uses Github."
      ],
      "default": "gh"
    },
    "auto_run": {
      "type": "bool",
      "description": [
        "If true (default), automatically starts kernel and runs code when ",
        "page with widgets is loaded. If false, will look for a DOM ",
        "element with id nbinteract and attach a click handler to run."
      ],
      "default": true
    }
    ```
    
    This means that we can safely deploy this to popular textbooks like Data
    8's as long as we turn off auto_run :).

--- packages/nbinteract-core/src/NbInteract.js
+++ packages/nbinteract-core/src/NbInteract.js
@@ -1,61 +1,54 @@
 import debounce from 'lodash.debounce'
 import once from 'lodash.once'
 
-import { Kernel, ServerConnection, KernelMessage } from '@jupyterlab/services'
+import { Kernel, ServerConnection } from '@jupyterlab/services'
 
+import * as util from './util.js'
 import BinderHub from './BinderHub'
 import { WidgetManager } from './manager'
 
-const baseToWsUrl = baseUrl =>
-  'wss:' +
-  baseUrl
-    .split(':')
-    .slice(1)
-    .join(':')
+const DEFAULT_PROVIDER = 'gh'
+const DEFAULT_SPEC = 'SamLau95/nbinteract-image/master'
 
-const WIDGET_MSG = 'application/vnd.jupyter.widget-view+json'
-
-const cellToCode = cell => cell.querySelector('.input_area').textContent
-const isWidgetCell = cell => cell.querySelector('.output_widget_view') !== null
-const cellToWidgetOutput = cell => cell.querySelector('.output_widget_view')
-const removeLoadingFromCell = cell => {
-  // Keep in sync with interact_template.tpl
-  const el = cell.querySelector('.js-widget-loading-indicator')
-  if (el) el.remove()
-}
-
-const isErrorMsg = msg => msg.msg_type === 'error'
-const msgToModel = (msg, manager) => {
-  if (!KernelMessage.isDisplayDataMsg(msg)) {
-    return
-  }
-
-  const widgetData = msg.content.data[WIDGET_MSG]
-  if (widgetData === undefined || widgetData.version_major !== 2) {
-    return
-  }
-
-  const model = manager.get_model(widgetData.model_id)
-  return model
-}
-
-// Class that runs notebook code and creates widgets
-//
-// The constructor takes in the following optional arguments:
-// {
-//   record_messages: "Debugging argument that records all messages sent by
-//      kernel. Will increase memory usage.",
-// }
+/**
+ * Main entry point for nbinteract.
+ *
+ * Class that runs notebook code and creates widgets.
+ */
 export default class NbInteract {
-  constructor({ record_messages } = { record_messages: false }) {
+  /**
+   * Initialize NbInteract. Does not start kernel until run() is called.
+   *
+   * @param {String} [spec] - Spec for BinderHub image. Must be in the format:
+   *     `${username}/${repo}/${branch}`. Uses nbinteract-image by default.
+   *
+   * @param {String} [provider] - BinderHub provider. Uses GitHub by default.
+   *
+   * @param {bool} [record_messages=false] - Debugging argument that records
+   *     all messages sent by kernel. Will increase memory usage!
+   */
+  constructor(
+    spec = DEFAULT_SPEC,
+    provider = DEFAULT_PROVIDER,
+    record_messages = false,
+  ) {
     this._getOrStartKernel = once(this._getOrStartKernel)
     this.run = debounce(this.run, 500, { leading: true, trailing: false })
-    this.binder = new BinderHub()
+    this.binder = new BinderHub(spec, provider)
     // Record messages for debugging
     this.messages = record_messages ? [] : false
   }
 
   run() {
+    if (!util.pageHasWidgets()) {
+      console.log('No widgets detected, stopping nbinteract.')
+
+      // Warm up kernel so the next run is faster
+      this._getOrStartKernel()
+
+      return
+    }
+
     // Generates a semi-random length-4 string. Just used for logging, so no
     // need to be super complicated.
     // From https://stackoverflow.com/a/8084248
@@ -69,7 +62,7 @@ export default class NbInteract {
 
         codeCells.forEach((cell, i) => {
           console.time(`cell_${i}_${run_id}`)
-          const code = cellToCode(cell)
+          const code = util.cellToCode(cell)
           const execution = kernel.requestExecute({ code })
 
           execution.onIOPub = msg => {
@@ -77,21 +70,21 @@ export default class NbInteract {
               this.messages.push(msg)
             }
 
-            if (isErrorMsg(msg)) {
-              console.error('Error in code run:', msg.content);
+            if (util.isErrorMsg(msg)) {
+              console.error('Error in code run:', msg.content)
             }
 
             // If we have a display message, display the widget.
-            if (!isWidgetCell(cell)) {
+            if (!util.isWidgetCell(cell)) {
               return
             }
 
-            const model = msgToModel(msg, manager)
+            const model = util.msgToModel(msg, manager)
             if (model !== undefined) {
-              const outputEl = cellToWidgetOutput(cell)
+              const outputEl = util.cellToWidgetOutput(cell)
               model.then(model => {
                 manager.display_model(msg, model, { el: outputEl })
-                removeLoadingFromCell(cell)
+                util.removeLoadingFromCell(cell)
                 console.timeEnd(`cell_${i}_${run_id}`)
               })
             }
@@ -115,7 +108,7 @@ export default class NbInteract {
       // Connect to the notebook webserver.
       const serverSettings = ServerConnection.makeSettings({
         baseUrl: url,
-        wsUrl: baseToWsUrl(url),
+        wsUrl: util.baseToWsUrl(url),
         token: token,
       })
       console.timeEnd('start_server')
