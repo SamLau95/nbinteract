d274836364f0b35a07bc8afd4f007d6384443303
Sam Lau <samlau95@gmail.com>
2018-02-10 11:59:48 -0800

    Python 0.0.22

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from os import path
 import sys
 
 # Package version
-version = '0.0.21'
+version = '0.0.22'
 
 here = path.abspath(path.dirname(__file__))
 
@@ -48,21 +48,16 @@ setup(
     description='Export interactive HTML pages from Jupyter Notebooks',
     long_description=long_description,
     url='https://github.com/SamLau95/nbinteract',
-
     author='Sam Lau',
     author_email='samlau95@gmail.com',
-
     license='MIT',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 3 - Alpha',
-
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-
         'License :: OSI Approved :: MIT License',
-
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
@@ -70,25 +65,19 @@ setup(
         'Framework :: Jupyter',
     ],
     keywords='jupyter nbconvert interact',
-
     packages=['nbinteract'],
-    package_data={
-        'nbinteract': ['*.tpl']
-    },
+    package_data={'nbinteract': ['*.tpl']},
     install_requires=install_requires,
-
     extras_require={
         'dev': ['check-manifest'],
         'test': ['pytest', 'coverage', 'coveralls'],
     },
-    cmdclass = {'test': PyTest},
+    cmdclass={'test': PyTest},
 
     # Add exporter to nbconvert CLI:
     # https://nbconvert.readthedocs.io/en/latest/external_exporters.html
     entry_points={
-        'nbconvert.exporters': [
-            'interact = nbinteract:InteractExporter',
-        ],
+        'nbconvert.exporters': ['interact = nbinteract:InteractExporter', ],
     },
 
     # Add nbinteract script
